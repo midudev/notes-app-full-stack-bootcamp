815fe7ea68c2b84cd93fb98885a437004dcb5b8f
Matti Luukkainen <mluukkai@iki.fi>
2020-01-15 17:07:12 +0200

    part2-1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2020-01-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2020-01-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2020-01-10T19:20:14.298Z',
+    important: true
+  }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
